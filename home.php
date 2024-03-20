45248a17f64f90b904e1d4c0e17f15f1f2f72093
Jeremy Benedicto <jeremybenedicto2020@gmail.com>
2024-03-20 10:52:54 +0800

    ss

--- home.php
+++ home.php
@@ -57,7 +57,7 @@ if(mysqli_num_rows($select) > 0){
       <h5><?php echo "Age: " .$fetch['age']; ?></h5>
       <a href="update_profile.php" class="btn">update profile</a>
       <a href="home.php?logout=<?php echo $user_id; ?>" class="delete-btn">logout</a>
-      <p>new <a href="login.php">login</a> or <a href="register.php">register</a></p>
+      
    </div>
 
 </div>
