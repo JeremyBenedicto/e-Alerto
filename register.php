7e30729ecb4c9b626a0053d115fa6c84737089b9
Jeremy Benedicto <jeremybenedicto2020@gmail.com>
2024-08-16 18:35:55 +0800

    ddd

--- register.php
+++ register.php
@@ -113,6 +113,7 @@ if(isset($_POST['submit'])){
          <option value="">Select Gender</option>
          <option value="Male">Male</option>
          <option value="Female">Female</option>
+         
       </select>
       <div class="pii">
     <label for="validphoto" class="d">
